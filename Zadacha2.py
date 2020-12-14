eb567c99e094a33831baba23c6ddfedba088f029
Dmitry <pentegov_92@mail.ru>
2020-12-14 21:24:56 +0300

    delete

--- Zadacha2.py
+++
@@ -1,7 +0,0 @@
-seconds = int(input("Введите количество секунд "))
-hours = seconds // 3600
-ost1 = seconds % 3600
-minutes = ost1 // 60
-ost2 = ost1 % 60
-print(f"{seconds} секунд - это {hours:02}:{minutes:02}:{ost2:02}")
-# для отображения секунд в последней строке
