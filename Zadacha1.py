4dcc04ef5cdbc9fd9c69c7e1a108c33126efdacf
Dmitry <pentegov_92@mail.ru>
2020-12-14 21:17:54 +0300

    tasks Lession1

---
+++ Zadacha1.py
@@ -0,0 +1,14 @@
+a = 10
+b = -1
+print(a, b)
+year = int(input("введите год рождения "))
+print(f'{year}')
+mes = int(input("введите месяц рождения "))
+print(f'{mes:02}')
+day = int(input("Введите день своего рождения "))
+print(f'{day:02}')
+print(f"Ваш месяц и год рождения: , {year} . {mes:02} . {day:02}")
+name = (input("введите свое имя "))
+print(name)
+surname = input("введите фамилию ")
+print("приятно познакомиться,", name, surname)
