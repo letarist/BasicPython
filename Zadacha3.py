8601087621c890b2454ea4acf9c7dd7462dcd734
Dmitry <pentegov_92@mail.ru>
2020-12-14 21:24:29 +0300

    delete

--- Zadacha3.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Введите n "))
-a = str(n)
-c = n + n
-b = n + n + n
-result = n + int(a) + int(b)
-print(result)
--- Zadacha4.py
+++
@@ -1,8 +0,0 @@
-num = int(input("введите целое положительное число "))
-ost = num % 10
-num = num // 10
-while num > 0:
-    if num % 10 > ost:
-        ost = num % 10
-        num = num // 10
-        print("Максимальное число ", ost)
