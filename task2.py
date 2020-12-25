68286a8a966ca655d84d19b96e3fe6b9b1c31a38
Dmitry <pentegov_92@mail.ru>
2020-12-25 18:54:02 +0300

    corret task2

--- task2.py
+++ task2.py
@@ -1,3 +1,3 @@
-spisok = [100, 50, 60, 20, 9, 10]
-spisok = [el for n, el in enumerate(spisok) if spisok[n - 1] < spisok[n]]
+spisok = [10, 50, 60, 20, 9, 10]
+spisok = [el for n, el in enumerate(spisok) if spisok[n-1] < spisok[n]]
 print(spisok)
