34f1f48e4b32839297edf0fac46fd281439eaacc
Dmitry <pentegov_92@mail.ru>
2021-01-15 17:25:47 +0300

    correct task3

--- task3.py
+++ task3.py
@@ -1,6 +1,7 @@
 class Сell:
     def __init__(self, count):
         self.count = count
+        print(f'Клетка с {self.count} ячейками ')
 
     def __add__(self, other):
         return self.count + other.count
@@ -18,7 +19,7 @@ class Сell:
             return 'Произведение меньше 0 '
 
     def __truediv__(self, other):
-            return 'один из аргументов является 0 '
+        return self.count / other.count
 
     def object(self, seb):
         return '\n'.join(['*' * seb for _ in range(self.count // seb)]) + '\n' + '*' * (self.count % seb)
@@ -27,7 +28,11 @@ class Сell:
 c1 = Сell(63)
 c2 = Сell(8)
 a = c1 - c2
+print(a)
 b = c1 * c2
+print(b)
 c = c1 / c2
+print(c)
 d = c1 + c2
+print(d)
 print(c1.object(5))
